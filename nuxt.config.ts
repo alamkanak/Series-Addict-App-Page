a6435937644f14036b374d56eaaccc77821c44b3
Raquib-ul Alam <alam.kanak@gmail.com>
2024-11-12 01:35:28 -0500

    Update github actions

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,5 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/image'],
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/image']
 })
